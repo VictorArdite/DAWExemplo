59f3e265592696b33169b86a84a10daf352ff3da
VictorArdite <victor_ardite@ceroca.cat>
2024-09-30 20:12:15 +0200

    Nau Disparant

--- Assets/Scrips/NauJugador.cs
+++ Assets/Scrips/NauJugador.cs
@@ -4,36 +4,60 @@ using UnityEngine;
 
 public class NauJugador : MonoBehaviour
 {
+    private float _vel;
+
+    private Vector2 minPantalla, maxPantalla;
+
+    [SerializeField]private GameObject prefabProjectil;
+
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8f;
-        minPantalla = Camera.main.ViewportToWorldPoint(Vector2(x0,y0));
-        maxPantalla = Camera.main.ViewportToWorldPoint(Vector2(x0,y0));
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
-        float meitarMidaImatgeX = GetComponnet<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x/2;
-        float meitarMidaImatgeY = GetComponnet<SpriteRenderer>().sprite.bounds.size.y * transform.localScale.y /2;
+        float meitatMidaImatgeX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x / 2;
+        float meitatMidaImatgeY = GetComponent<SpriteRenderer>().sprite.bounds.size.y * transform.localScale.y / 2;
 
-        minPantalla.x = minPantalla.x +0.75f;
-        maxPantalla.x = maxPantalla.x - 0.75f;
-        minPantalla.y += meitarMidaImatgeY;
-        maxPantalla.y -= meitarMidaImatgeY;
+        minPantalla.x = minPantalla.x + 0.75f;
+        maxPantalla.x = maxPantalla.x - 0.75F;
+        minPantalla.y += 0.75f;
+        maxPantalla.y -= 0.75f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoureNau();
+        DisparaProjectil();
+        
+    }
+
+    private void DisparaProjectil()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject projectil = Instantiate(prefabProjectil);
+            projectil.transform.position = transform.position;
+        }
+    }
+    private void MoureNau()
+    {
+
         float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccioIndicadaY = Input.GetAxisRaw("Vertical");
 
-        Vector2 direccioIndicada = new Vector2(direccioIndicadaX,direccioIndicadaY).normalized;
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
 
         Vector2 novaPos = transform.position;
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
 
-        novaPos.x = Math.Clamp(novaPos.x,minPantalla.x,maxPantalla.x);
-        novaPos.x = Math.Clamp(novaPos.y,minPantalla.y,maxPantalla.y);
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
 
-        transform.position=novaPos;
+        transform.position = novaPos;
     }
 }
+
+
